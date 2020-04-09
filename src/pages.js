98f406dbec9bdda7116d07c994642b30cb1fd75a
junping.wu <9700616@qq.com>
2020-04-09 16:15:17 +0800

    ++

--- src/pages.js
+++ src/pages.js
@@ -8,18 +8,25 @@ module.exports=(pagesJson, loader)=>{
 
     // 对pages去重并且设置首页
     function removeDuplicationAndSetIndexPage(pages = [], indexPath = pages[0] && pages[0].path || ''){
-        let uniquePageMap = {}, resultPages=[]
+        let uniquePageMap = {}, resultPages=[], indexPage=[]
 
         // 去重
         pages.forEach((page) => {
             uniquePageMap[page.path] = page
         })
+
+        // 抽出首页
+        indexPage.push(uniquePageMap[indexPath])
+        delete uniquePageMap[indexPath]
+
         for (let i in uniquePageMap){
             resultPages.push(uniquePageMap[i])
         }
 
-        // 将indexPath排序到数组首位
-        return resultPages.sort((page) =>  page.path === indexPath && -1)
+        return [
+            ...indexPage,
+            ...resultPages
+        ]
     }
 
     return {
@@ -27,13 +34,15 @@ module.exports=(pagesJson, loader)=>{
         ...pagesJson,
         // 可自行对pages进行排序和去重，可以在元素里加上特殊的key作为排序规则
         pages: removeDuplicationAndSetIndexPage([
-            ...basePages,
-            ...hotRequire('./page_modules/tabbar.js'),
-            // 故意重复引入，用来验证去重方法
-            ...hotRequire('./page_modules/tabbar.js'),
-            ...hotRequire('./page_modules/component.js'),
-            ...hotRequire('./page_modules/appPlus.js')
-        ]),
+                ...basePages,
+                ...hotRequire('./page_modules/tabbar.js'),
+                // 故意重复引入，用来验证去重方法
+                ...hotRequire('./page_modules/tabbar.js'),
+                ...hotRequire('./page_modules/component.js'),
+                ...hotRequire('./page_modules/appPlus.js')
+            ],
+            // 设置首页
+            'pages/component/form/form'),
         subPackages: [
             ...baseSubPackages,
             ...hotRequire('./subpackage_modules/api.js'),
