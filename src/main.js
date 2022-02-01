db8671193506dfc6e72d424b206a8c06d0cabdb5
devilwjp <9700616@qq.com>
2022-02-01 22:58:03 +0800

    ++++

--- src/main.js
+++ src/main.js
@@ -5,7 +5,6 @@ import pageHead from './components/page-head.vue'
 import pageFoot from './components/page-foot.vue'
 import uLink from '@/components/uLink.vue'
 import store from './store'
-import './router'
 
 Vue.config.productionTip = false
 
